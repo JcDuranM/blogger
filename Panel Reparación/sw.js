1f63b9bd9f9ba37ad36b9aa37bd6c731b36aee4f
JcDuranM <72593176+JcDuranM@users.noreply.github.com>
2025-05-21 14:29:34 -0600

    Update sw.js

--- Panel Reparación/sw.js
+++ Panel Reparación/sw.js
@@ -1,20 +1,48 @@
-const CACHE_NAME = 'panel-jcduranm-v1';
+// sw.js - Service Worker
+
+const CACHE_NAME = 'panel-reparacion-cache-v1';
 const urlsToCache = [
   '/',
-  'https://panel.jcduranm.com/',
-  'https://fonts.googleapis.com/css?family=Roboto'
+  'https://cdn.jsdelivr.net/gh/JcDuranM/blogger@main/Panel%20Reparaci%C3%B3n/index.html',
+  'https://cdn.jsdelivr.net/gh/JcDuranM/blogger@main/Panel%20Reparaci%C3%B3n/manifest.json',
+  'https://cdn.jsdelivr.net/gh/JcDuranM/blogger@main/Panel%20Reparaci%C3%B3n/icon-192.png',
+  'https://cdn.jsdelivr.net/gh/JcDuranM/blogger@main/Panel%20Reparaci%C3%B3n/icon-512.png',
+  // Agregar cualquier otro archivo relevante, como HTML, CSS o JS adicionales
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => {
+        console.log('Archivos en caché durante la instalación del service worker');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request);
+      })
   );
 });
