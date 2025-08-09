3ee019c04e0aa7c74c766fe7d6dc2b7916c21ea3
JcDuranM <72593176+JcDuranM@users.noreply.github.com>
2025-08-09 16:30:41 -0600

    Create service-worker.js

---
+++ lashes/service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'reserva-alberca-v1';
+const urlsToCache = [
+  '/',
+  'https://fonts.googleapis.com/css?family=Segoe+UI',
+  '/' // Cambia a tu URL real si quieres
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
